268b90432522d0d3f5e1088abee334ba2a908671
nikozorica <56315996+nikozorica@users.noreply.github.com>
2019-10-10 11:31:45 -0400

    Create Mysite.js

---
+++ Mysite.js
@@ -0,0 +1,6 @@
+function changetext()
+{
+	var changedTxt = "Note This Version Does Not Save Your Data!"
+	document.getElementById("ChangeMe").innerHTML = changedTxt;
+	document.getElementById("ChangeMe").style.color = "#FFDBE8";
+}
